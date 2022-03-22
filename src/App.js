5d28cf401ff745d7470f3e727614782ceac157da
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-22 09:26:58 +0100

    Include all buttons

--- src/App.js
+++ src/App.js
@@ -2,22 +2,55 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [ firstNumber, setFirstNumber ] = useState(null);
+  const [ firstNumber, setFirstNumber ] = useState(0);
+  const [ secondNumber, setSecondNumber ] = useState(0);
+  let [ result, setResult ] = useState(0);
+
 
   function changeFirstNumberHandler (event) {
     setFirstNumber(event.target.value);
   }
 
+  function changeSecondNumberHandler (event) {
+    setSecondNumber(event.target.value);
+  }
+
   useEffect(
     ()=>{
       console.log("firstNumber state:", firstNumber);
+      console.log("secondNumber state:", secondNumber);
     }
   )
+
+  function addHandler () {
+    result=parseFloat(firstNumber)+parseFloat(secondNumber);
+    setResult(result);
+  }
+
+  function restHandler () {
+    result=parseFloat(firstNumber)-parseFloat(secondNumber);
+    setResult(result);
+  }
+  function multiplyHandler () {
+    result=parseFloat(firstNumber)*parseFloat(secondNumber);
+    setResult(result);
+  }
+  function divideHandler () {
+    result=parseFloat(firstNumber)/parseFloat(secondNumber);
+    setResult(result);
+  }
 
   return (
     <>
       <h1>Calculadora</h1>
-      <input type="text" value={firstNumber} onChange={changeFirstNumberHandler}/>
+      <input type="text" value={firstNumber} onChange={changeFirstNumberHandler}/><br/>
+      <input type="text" value={secondNumber} onChange={changeSecondNumberHandler}/>
+
+      <p>{result}</p>
+      <button onClick={addHandler }>sumar</button>
+      <button onClick={restHandler }>restar</button>
+      <button onClick={multiplyHandler }>multiplicar</button>
+      <button onClick={divideHandler }>dividir</button>
     </>
   );
 }
