301611fce43a32e6af6f3a07f4c68843276fe0ad
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-04-20 09:58:22 +0200

    Update History.jsx

--- src/components/history/History.jsx
+++ src/components/history/History.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState, useRef } from 'react';
 import './History.css';
 
-function History({ results }) {
+function History({results}) {
 
     const liArray = results.map(
         element => <li>{element}</li>
     )
-
-
     return (
         <ul>
             {liArray}
